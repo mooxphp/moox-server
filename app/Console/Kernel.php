073681937bb8b574637782786dabfb596aa4511b
Alf Drollinger <alf@drollinger.info>
2024-01-31 09:40:23 +0100

    Manual Backup w-i-p

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,9 +4,17 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Spatie\BackupServer\Commands\CreateBackupCommand;
 
 class Kernel extends ConsoleKernel
 {
+    /**
+     * Add Moox Backup Server command to the Artisan console
+     */
+    protected $commands = [
+        CreateBackupCommand::class,
+    ];
+
     /**
      * Define the application's command schedule.
      */
